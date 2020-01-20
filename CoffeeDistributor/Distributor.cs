59a50068a42bb1527947a19b1eb53ce5596b08d2
justcris <cristian.scapin654@icloud.com>
2020-01-20 18:36:45 +0100

    initialized project

---
+++ CoffeeDistributor/Distributor.cs
@@ -0,0 +1,17 @@
+namespace CoffeeDistributor
+{
+    public class Distributor
+    {
+        private int maxcoffee;
+        private int maxThe;
+        private int maxCups;
+        private int maxSugar;
+        private int coffeeCapsules;
+        private int theCapsules;
+        private int cups;
+        private int sugarCubes;
+        private string psw;
+        private bool outOfOrder;
+        private int[,,] supplied;
+    }
+}
