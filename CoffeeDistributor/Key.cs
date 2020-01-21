b2420a21c804abb02d2e7388fd9d6ce54c57ce49
JustCris654 <59602675+JustCris654@users.noreply.github.com>
2020-01-21 13:03:31 +0100

    Add files via upload

---
+++ CoffeeDistributor/Key.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeDistributor {
+    public class Key {
+        private double _balance;
+
+
+        public Key(double balance) {
+            _balance = balance;
+        }
+
+        public bool buyDrink(double cost) {
+            if (_balance < cost) return false;
+            _balance -= cost;
+            return true;
+        }
+
+        public double Balance => _balance;
+
+    }
+}
